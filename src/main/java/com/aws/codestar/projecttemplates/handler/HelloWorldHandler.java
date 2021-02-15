0b51309dd9af0643196456c7887659529dc4d7c3
S0ph1ia <badalovasophia@gmail.com>
2021-02-15 12:49:28 +0000

    added factorial code

--- src/main/java/com/aws/codestar/projecttemplates/handler/HelloWorldHandler.java
+++ src/main/java/com/aws/codestar/projecttemplates/handler/HelloWorldHandler.java
@@ -6,6 +6,7 @@ import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.aws.codestar.projecttemplates.GatewayResponse;
 import org.json.JSONObject;
 
+import java.math.BigInteger;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -17,6 +18,17 @@ public class HelloWorldHandler implements RequestHandler<Object, Object> {
     public Object handleRequest(final Object input, final Context context) {
         Map<String, String> headers = new HashMap<>();
         headers.put("Content-Type", "application/json");
-        return new GatewayResponse(new JSONObject().put("Output", "Hello World!").toString(), headers, 200);
+
+        return new GatewayResponse(new JSONObject().put("Result is: ", factorial(new BigInteger("4000"))).toString(), headers, 200);
+    }
+
+    private BigInteger factorial(BigInteger num) {
+        BigInteger total;
+        if (num.equals(new BigInteger("1"))) {
+            return new BigInteger("1");
+        } else {
+            total = num.multiply(factorial(num.subtract(new BigInteger("1"))));
+        }
+        return total;
     }
 }
